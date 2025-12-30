0ca936bf8058af03646290c7f319fbe5d7b1de20
gadreeldias101103-cmd <gadreeldias101103@gmail.com>
2025-12-30 13:28:36 -0300

    Create wallet.js

---
+++ wallet.js
@@ -0,0 +1,32 @@
+const API = "https://SEU_BACKEND.ct.ws/api";
+
+BlackCatPay.setPublicKey("SUA_PUBLIC_KEY");
+
+async function depositar(){
+  const token = localStorage.getItem("token");
+
+  const card = {
+    number: numero.value,
+    holderName: nome.value,
+    expirationMonth: mes.value,
+    expirationYear: ano.value,
+    cvv: cvv.value
+  };
+
+  const cardToken = await BlackCatPay.tokenize(card);
+
+  const res = await fetch(`${API}/cartao_deposito.php`, {
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      "Authorization":"Bearer "+token
+    },
+    body: JSON.stringify({
+      valor: valor.value,
+      card_token: cardToken.id
+    })
+  });
+
+  const data = await res.json();
+  alert(data.message);
+}
