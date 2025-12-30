6a668c14aec88a68da38358ba557b8491b9b96c4
gadreeldias101103-cmd <gadreeldias101103@gmail.com>
2025-12-30 14:08:19 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,33 +3,46 @@ const API = "https://viraeraul.ct.ws/api";
 function login() {
   fetch(`${API}/login.php`, {
     method: "POST",
-    headers: {"Content-Type":"application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       email: email.value,
       senha: senha.value
     })
   })
-  .then(r=>r.json())
-  .then(d=>{
-    if(d.token){
+  .then(r => r.json())
+  .then(d => {
+    if (d.token) {
       localStorage.setItem("token", d.token);
-      window.location = "wallet.html";
-    } else alert("Login inválido");
-  });
+      location.href = "wallet.html";
+    } else {
+      alert(d.error || "Login inválido");
+    }
+  })
+  .catch(() => alert("Erro de conexão"));
 }
 
 function register() {
   fetch(`${API}/register.php`, {
     method: "POST",
-    headers: {"Content-Type":"application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       email: email.value,
       senha: senha.value
     })
-  }).then(()=>location.href="index.html");
+  })
+  .then(r => r.json())
+  .then(d => {
+    if (d.token) {
+      localStorage.setItem("token", d.token);
+      location.href = "wallet.html";
+    } else {
+      alert(d.error || "Erro ao cadastrar");
+    }
+  })
+  .catch(() => alert("Erro de conexão"));
 }
 
-function logout(){
+function logout() {
   localStorage.removeItem("token");
-  location.href="index.html";
+  location.href = "index.html";
 }
